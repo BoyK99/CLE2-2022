f04cc82e9f1aba394bcb42bbf442b71dd14ca86b
BoyK99 <blgkik@hotmail.com>
2022-11-15 10:10:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+echo('hi');
+//    DB naam: reservering2022
+//    DB user: root
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/335a0c3dec.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Hoofdpagina</title>
+</head>
+<body>
+
+<section class="hero is-fullheight">
+    <div class="hero-body has-text-centered">
+        <div class="container">
+            Hier komt het inlog systeem - WIP
+        </div>
+    </div>
+</section>
+</body>
+</html>
